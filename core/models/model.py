471c755adbd1a7a397288e5a9c6f347fe07bfe6a
Kai17-a <k.s25skt@gmail.com>
2025-12-22 00:49:33 +0900

    feat: SQLModel追加

---
+++ core/models/model.py
@@ -0,0 +1,50 @@
+from enum import Enum
+from sqlmodel import SQLModel, Field
+
+
+class WebhookType(Enum):
+    DISCORD = "discord"
+    SLACK = "slack"
+    TEAMS = "teams"
+
+
+class WebsiteType(Enum):
+    RSS = "rss"
+    SCRAPING = "scraping"
+
+
+class Article(SQLModel, table=True):
+    __tablename__ = "articles"
+
+    id: int = Field(default=None, primary_key=True)
+    hash: str = Field(index=True)
+    title: str
+    url: str
+    site_name: str
+    created_at: str
+
+
+class Webhook(SQLModel, table=True):
+    __tablename__ = "webhooks"
+
+    id: int = Field(default=None, primary_key=True)
+    name: str = Field(unique=True)
+    endpoint: str = Field(unique=True)
+    service_type: WebhookType
+    is_active: bool = Field(default=True)
+    created_at: str
+
+
+class Website(SQLModel, table=True):
+    __tablename__ = "websites"
+
+    id: int = Field(default=None, primary_key=True)
+    name: str = Field(unique=True)
+    type: WebsiteType
+    url: str = Field(unique=True)
+    avatar: str | None = Field(default=None, unique=True)
+    selector: str | None = Field(default=None)
+    is_active: bool = Field(default=True)
+    needs_translation: bool = Field(default=False)
+    target_webhook_ids: str | None = Field(default=None)
+    created_at: str
