cabd5a7b06a421edb57269e8423effa44b0e2c1d
Kai17-a <k.s25skt@gmail.com>
2026-01-03 00:33:43 +0900

    feat: webhookサービス追加

---
+++ core/services/webhook.py
@@ -0,0 +1,52 @@
+from sqlmodel import Session
+
+from core.config import logger
+from core.models.model import Webhook, Website
+from core.repositories.webhook import WebhookRepository
+
+
+class WebhookService:
+    """Webhookサービスの基底クラス."""
+
+    def __init__(self, session: Session) -> None:
+        self.repository = WebhookRepository(session)
+
+    def check_default_webhooks(self) -> bool:
+        """デフォルトのWebhookを初期化."""
+        try:
+            webhooks = self.get_webhook()
+            if not webhooks:
+                logger.info(
+                    "Webhookが設定されていません。データベースにWebhookを追加してください。",
+                )
+                return False
+        except Exception:
+            logger.exception("Webhook取得エラー")
+            raise
+        return True
+
+    def get_webhook(self) -> list[Webhook]:
+        """Webhook情報を登録."""
+        webhooks = []
+        try:
+            webhooks = self.repository.get_all()
+        except Exception:
+            logger.exception("Webhook取得エラー")
+            raise
+        else:
+            return webhooks
+
+    def get_target_webhook(self, website: Website) -> list[Webhook]:
+        """送信対象Webhook情報を登録."""
+        webhooks = self.get_webhook()
+
+        if not hasattr(website, "target_webhook_ids") or not website.target_webhook_ids:
+            return webhooks
+
+        target_ids = [
+            target_webhook_id.strip()
+            for target_webhook_id in website.target_webhook_ids.split(",")
+            if target_webhook_id.strip()
+        ]
+
+        return [webhook for webhook in webhooks if str(webhook.id) in target_ids]
