d06d9b43a739780afce636f884092c5eb55c888f
Kai17-a <k.s25skt@gmail.com>
2026-01-04 00:10:38 +0900

    feat: MAX_ARTICLES_PER_SITEを10に変更

--- core/config.py
+++ core/config.py
@@ -3,12 +3,16 @@
 import logging
 
 logging.basicConfig(
+    # filename="app.log",
+    # filemode="w",
+    # encoding="utf-8",
     level=logging.INFO,
     format="%(asctime)s - %(levelname)s - %(message)s",
+    # force=True,
 )
 logger = logging.getLogger(__name__)
 
 
 TRANSLATION_API_SUCCESS_STATUS = 200
 TRANSLATION_API_URL = "https://api.mymemory.translated.net/get"
-MAX_ARTICLES_PER_SITE = 15
+MAX_ARTICLES_PER_SITE = 10
