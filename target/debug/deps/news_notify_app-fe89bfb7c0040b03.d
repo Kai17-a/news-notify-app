2397a34f4203f30f317fef51b7c69335c28ccf10
Kai17-a <k.s25skt@gmail.com>
2026-02-20 00:43:50 +0900

    feat: Delete Python resource and init Rust

---
+++ target/debug/deps/news_notify_app-fe89bfb7c0040b03.d
@@ -0,0 +1,5 @@
+/home/kaito/workspaces/news-notify-app/target/debug/deps/news_notify_app-fe89bfb7c0040b03.d: src/main.rs
+
+/home/kaito/workspaces/news-notify-app/target/debug/deps/news_notify_app-fe89bfb7c0040b03: src/main.rs
+
+src/main.rs:
---
+++ target/debug/news-notify-app.d
@@ -0,0 +1,1 @@
+/home/kaito/workspaces/news-notify-app/target/debug/news-notify-app: /home/kaito/workspaces/news-notify-app/src/main.rs
